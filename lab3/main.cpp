253c67da99e78c25007e05ac13a5e9de0b62d515
timetodosomehomework <58882714+timetodosomehomework@users.noreply.github.com>
2020-12-27 17:47:08 +0300

    Update main.cpp

--- lab3/main.cpp
+++ lab3/main.cpp
@@ -9,15 +9,14 @@ int main()
 	{
 		heap.insert(i + 2);
 	}
-	/*heap.remove(6);*/
-	Iterator* heap_iterator_bft = heap.create_bft_iterator();
+	Iterator* heap_iterator_bft = heap.create_bft_iterator(); //bft
 	while (heap_iterator_bft->hasnext())
 	{
 		cout << heap_iterator_bft->next_bft() << '\t';
 	}
 	cout << endl;
-	/*heap.remove(0);*/
-	Iterator* heap_iterator_dft = heap.create_dft_iterator();
+	heap.remove(6); //delete 6 element in heap
+	Iterator* heap_iterator_dft = heap.create_dft_iterator(); //dft
 	while (heap_iterator_dft->hasnext())
 	{
 		cout << heap_iterator_dft->next_dft() << '\t';
